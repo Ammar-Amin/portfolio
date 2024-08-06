9f1af0a5010273e057b4b917a2677a0ae7452fd7
Ammar-Amin <ammaramin5253@gmail.com>
2024-08-06 21:26:18 +0530

    add projects data

---
+++ src/assets/index.js
@@ -0,0 +1,26 @@
+import atg from './atg.png';
+import radiant from './radiant.png'
+import blog from './blog.png'
+import cal from './cal.png'
+import weather from './weather.png'
+import estate from './estate.png'
+import qid from './qid.png'
+import luma from './luma.png'
+import mole from './mole.png'
+import van from './van.png'
+import todo from './todo.png'
+
+
+export {
+    atg,
+    radiant,
+    blog,
+    cal,
+    weather,
+    estate,
+    qid,
+    luma,
+    mole,
+    van,
+    todo,
+}
---
+++ src/constants/index.js
@@ -0,0 +1,115 @@
+import { atg, blog, cal, estate, luma, mole, qid, radiant, todo, van, weather } from "../assets"
+
+
+export const mega = [
+    {
+        title: "AminEstate",
+        img: estate,
+        desc: "Amin Estate is the best place to find your next perfect place to live, We have a wide range of properties for you to choose from.",
+        skills: ["MongoDB", "Express.js", "React.js", "Redux", "firebase", "Nodejs", "Insomnia"],
+        live: "https://estate-mern-0kti.onrender.com",
+        github: "https://github.com/Ammar-Amin/Estate-MERN"
+    },
+    {
+        title: "Blog Application",
+        img: blog,
+        desc: "",
+        skills: ["React", "ReactHookForms", "TailwindCss", "Cloudinary", "Appwrite", "Tinymce"],
+        live: "https://blog-app-05.netlify.app/",
+        github: "https://github.com/Ammar-Amin/Blog-App"
+    },
+]
+
+export const clones = [
+    {
+        title: "Qid Clone",
+        img: qid,
+        desc: "Completed a website clone assignment in 3 days, replicating the whole website of a prominent platform.",
+        skills: ["React", "TailwindCSS", "ReactRouterDom"],
+        live: "https://qid-ui-by-ammar.vercel.app/",
+        github: "https://github.com/Ammar-Amin/qid-ui"
+    },
+    {
+        title: "LU.MA Replica",
+        img: luma,
+        desc: "",
+        skills: ["React", "TailwindCSS", "Javascript",],
+        live: "",
+        github: "https://github.com/Ammar-Amin/lu.ma_Replica"
+    },
+]
+
+export const normal = [
+    {
+        title: "Van Dwelling",
+        img: van,
+        desc: "",
+        skills: [],
+        live: "https://van-rental-web.vercel.app/",
+        github: "https://github.com/Ammar-Amin/Van-LifeStyle"
+    },
+    {
+        title: "Todo Application",
+        img: todo,
+        desc: "",
+        skills: [],
+        live: "https://quadb-todo.netlify.app/",
+        github: ""
+    },
+]
+
+
+export const basic = [
+    {
+        title: "Weather App",
+        img: weather,
+        desc: "",
+        skills: ["React", "TailwindCSS Components", "OpenWeatherAPI"],
+        live: "https://weather-forecst.netlify.app",
+        github: "https://github.com/Ammar-Amin/"
+    },
+    {
+        title: "ATG World UI",
+        img: atg,
+        desc: "",
+        skills: ["React", "TailwindCSS"],
+        live: "https://atg-world-web.netlify.app/",
+        github: "https://github.com/Ammar-Amin/ATG-World-UI"
+    },
+    {
+        title: "Internship Assignment",
+        img: radiant,
+        desc: "Successfully completed an assignment form Radiant Riches, ui clone for page builders website",
+        skills: ["React", "TailwindCSS"],
+        live: "https://main--ammar-amin-radiant-riches.netlify.app/",
+        github: "https://github.com/Ammar-Amin/Internship-Assignment-Radiant-Riches-LCC"
+    },
+]
+
+export const games = [
+    {
+        title: "Whack a Mole",
+        img: mole,
+        desc: "",
+        skills: ["HTML", "CSS", "Javascript"],
+        live: "https://random-mole-generating-game.vercel.app/",
+        github: "https://github.com/Ammar-Amin/Random-Mole-Game"
+    },
+    {
+        title: "Personal Calculator",
+        img: cal,
+        desc: "",
+        skills: ["HTML", "CSS", "Javascript"],
+        live: "https://ammars-calculator.netlify.app",
+        github: "https://github.com/Ammar-Amin/"
+    },
+]
+
+var obj = {
+    title: "",
+    img: "",
+    desc: "",
+    skills: [],
+    live: "",
+    github: "https://github.com/Ammar-Amin/"
+}
